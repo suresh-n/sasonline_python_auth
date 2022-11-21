97ee2e974803f4c97ba337389e2cfffb8297e0a3
Suresh Nandagopal <sureshcbe5@gmail.com>
2022-11-21 15:51:31 +0530

    Update Generate_token.py

--- Generate_token.py
+++ Generate_token.py
@@ -1,4 +1,4 @@
-
+#WIP
 
 client_id = 'SAS-CLIENT'
 client_secret = 'xxxxxxxxxxxxxxxxxxx'
@@ -12,17 +12,10 @@ from authlib.integrations.httpx_client import AsyncOAuth2Client
 
 client = AsyncOAuth2Client(client_id, client_secret, scope=scope)
 
-authorization_endpoint = 'https://alphaapi.sasonline.in/oauth2/auth'
 
-uri, state = client.create_authorization_url(authorization_endpoint)
-
-print(uri)
-
-
-sleep 4
 # copy the auth URL to browser and paster the response to authorization_response
 
-authorization_response = 'paste the the auth reponse code'
+authorization_response = ''  #paste the the auth reponse code
 token_endpoint = 'https://alphaapi.sasonline.in/oauth2/token'
 token = client.fetch_token(token_endpoint, authorization_response=authorization_response)
 print(token)
